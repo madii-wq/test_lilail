7b7f749976eeb5219a9c89596d27aa65e9e8358c
Madiyar <madiaraza2002@gmail.com>
2023-10-16 08:42:18 +0500

    Added files

---
+++ !Project.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    int index, urnum, digit;
+    cin >> urnum;
+    int arr[urnum];
+    for (int i = 0; i < urnum; i++) {
+        
+        cin >> arr[i];
+    }
+    for (int i = 1; i < urnum; i++) {
+        index = arr[0];
+        if (arr[0] < arr[i]) {
+
+            index = arr[i];
+
+        }
+    }
+    cout << index;
+    return 0;
+}
